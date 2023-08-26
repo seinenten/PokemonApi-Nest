d8de19a54a80dec3fee54cb56db9d3765f16f137
jorge <20200707@uthh.edu.mx>
2023-08-26 10:48:03 -0600

    Seed
    Antes de insertar por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(){}
+
+  async executeSeed(){
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    
+    data.results.forEach( ({ name, url })=> {
+
+      const segments = url.split('/');
+      //console.log(segments);
+
+      const no:number = Number(segments[ segments.length - 2 ]);
+
+      console.log({ name, no });
+      
+
+    })
+    
+    
+    
+    return data.results;
+  }
+
+
+}
